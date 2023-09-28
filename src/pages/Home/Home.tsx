5c4532901fc70bf22d8c0cd033125920e648b113
Leonardo <98045798+joseleoc@users.noreply.github.com>
2023-09-28 11:58:01 -0400

    Integrate jest (#2)
    * integrate jest with expo
    
    * home test example
    
    * ajustes PR
    
    * build(package-json): update react native
    
    ---------
    
    Co-authored-by: Raidenmystery <rafaeljoserivascalderon@gmail.com>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { REACT_APP_BASE_URL } from "@env";
 import { useGetUsers } from "@/hooks/example/service-example";
-import { Colors } from "@/styles/colors";
+import { styles } from "./Home.styles";
 
 export function Home(): React.JSX.Element {
   const { data: users } = useGetUsers();
@@ -33,24 +33,3 @@ export function Home(): React.JSX.Element {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: Colors.white,
-    margin: 40,
-  },
-  title: { fontSize: 32, fontWeight: "bold", margin: 20 },
-  sectionsContainer: {
-    display: "flex",
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-between",
-    flexWrap: "wrap",
-    gap: 26,
-  },
-  subHeader: { fontSize: 16, margin: 10, fontWeight: "500", textAlign: "center" },
-  listItem: { margin: 5 },
-});
